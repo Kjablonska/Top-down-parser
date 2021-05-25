1c5dcf9a824a0c970ef816dfbc2d2796553115e7
Kjablonska <k.jablonska8@gmail.com>
2021-05-25 18:13:08 +0200

    add parser

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from input_handler import input_handler
+from parser import parser
 
 def main():
     CFG, input_string = input_handler()
+    parser(CFG, input_string)
 
 
 main()
--- parser.py
+++ parser.py
@@ -1,25 +1,24 @@
 from parse_tree import Node
 
-CFG = {
-    "S": ["A", "$"],
-    "A": ["b", "c"]
-}
-
-# Works only in very simple cases.
-
-# TODO:
-# Add backtrcking.
-
-def parser():
-    rules = CFG["S"]
-    input = "$"
-
-    out, parse_tree = rec_parse("S", input, 0, "", Node("S"), [])
-    print(out)
-    print_parse_tree(parse_tree)
+# CFG = {
+#     "S": ["A", "$"],
+#     "A": ["b", "c"]
+# }
 
 
-def rec_parse(nonterminal, input, input_iter, out, node, parse_tree):
+def parser(CFG, input):
+    print(CFG['S'])
+    rules = CFG['S']
+    input = input[:-1]
+
+    if CFG != {}:
+        out, parse_tree = rec_parse(CFG, "S", input, 0, "", Node("S"), [])
+        print(out)
+        print_parse_tree(parse_tree)
+
+
+def rec_parse(CFG, nonterminal, input, input_iter, out, node, parse_tree):
+    print(CFG)
     rules = CFG[nonterminal]
     match = False
 
@@ -40,7 +39,7 @@ def rec_parse(nonterminal, input, input_iter, out, node, parse_tree):
             print("current input " + input[input_iter])
             if rule[i].isupper() == True:
                 print("out before rec", out)
-                out, parse_tree = rec_parse(rule[i], input, input_iter, out, Node(rule[i]), parse_tree)
+                out, parse_tree = rec_parse(CFG, rule[i], input, input_iter, out, Node(rule[i]), parse_tree)
                 print("out after rec ", out)
             else:
                 if rule[i] == input[input_iter]:
@@ -95,4 +94,4 @@ def print_parse_tree(parse_tree):
 
     print("----------------------------------------------------------------------\n")
 
-parser()
+# parser()
