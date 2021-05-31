ff7661253fcdeb247b9f6ebfd612534123535f32
Kjablonska <k.jablonska8@gmail.com>
2021-05-31 15:31:15 +0200

    improve backtracking

--- parser.py
+++ parser.py
@@ -4,16 +4,16 @@ from parse_tree import Node
 def parser(CFG, input):
     print(CFG)
     rules = CFG['S']
-    input = input[:-1]
+    # input = input[:-1]
 
     if CFG != {}:
-        out, parse_tree = rec_parse(CFG, "S", input, 0, "", Node("S"), [])
-        print("End of input parsing")
+        out, parse_tree, match = rec_parse(CFG, "S", input, 0, "", Node("S"), [], 0)
+        print("\n\nEnd of input parsing")
         print(out)
         print_parse_tree(parse_tree)
 
 
-def rec_parse(CFG, nonterminal, input, input_iter, out, node, parse_tree):
+def rec_parse(CFG, nonterminal, input, input_iter, out, node, parse_tree, matched):
     rules = CFG[nonterminal]
     match = False
 
@@ -24,8 +24,9 @@ def rec_parse(CFG, nonterminal, input, input_iter, out, node, parse_tree):
     parse_tree.append(node)
 
     for rule in rules:
-        print(rule)
+        print("rule", rule)
         match = False
+        matched = 0
         for i in range(len(rule)):
             node.add_children(rule[i])
             print_parse_tree(parse_tree)
@@ -33,40 +34,50 @@ def rec_parse(CFG, nonterminal, input, input_iter, out, node, parse_tree):
             print("curr rule " + rule)
             print("current input " + input[input_iter])
             if rule[i].isupper() == True:
-                print("out before rec", out)
-                out, parse_tree = rec_parse(CFG, rule[i], input, input_iter, out, Node(rule[i]), parse_tree)
-                print("out after rec ", out)
+                out, parse_tree, match = rec_parse(CFG, rule[i], input, input_iter, out, Node(rule[i]), parse_tree, matched)
+                print("rec end", out, input[:-1], match)
+                if out == input[:-1] and match == True:
+                    match = True
+                    print("out rec match")
+                    return out, parse_tree, match
             else:
                 if rule[i] == input[input_iter]:
                     out = out + rule[i]
                     input_iter += 1
+                    matched += 1
+                    match = True
+                elif rule[i] == '$':
+                    match = True
+                    continue
                 else:
                     # backtracking:
-                    for el in node.children:
+                    print("\t\tBacktracking")
+                    print(input_iter)
+                    print(input[input_iter])
+                    print(input)
+
+                    for el in range(matched):
                         out = out.replace(el, "")
                         input_iter = input_iter - 1
+
                     node.remove_children()
                     match = False
-                    print(out)
-                    print("Backtracking")
+                    matched = 0
                     break
 
-            if out == input:
-                match = True
-                print("match")
-                return out, parse_tree
+        if out == input[:-1] and match == True:
+            match = True
+            print("inner loop match")
+            return out, parse_tree, match
 
 
-        if match == True:
-            return out, parse_tree
+        if out == input[:-1] and match == True:
+            return out, parse_tree, match
         else:
             node.remove_children()
-            # if node.children == [] and len(parse_tree) > 0:
-            #     parse_tree.pop()
-            # input_iter = 0
 
     print("parsing ends", out)
-    return out, parse_tree
+    return out, parse_tree, match
 
 
 def parse_tmp(rule, input, input_iter, out):
